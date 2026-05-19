4d0512e961cceba12769ed3f5e532b3d19d56256
Jordan Soo <jordan@tatsu.works>
2026-05-19 16:20:06 +0800

    feat: deps setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medibot',
+    packages=find_packages(),
+    version='0.1.0',
+    description='medibot project.',
+    author='soo yen yih',
+    author_email='yenyih.soo@gmail.com',
+    license='MIT',
+    install_requires=[]
+)
